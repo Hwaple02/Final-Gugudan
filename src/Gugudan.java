bbabb08ecba3cc5fafd9a94635d4421919f33b44
Ben Kang <Ben Kang@DESKTOP-6RPFV0J>
2022-03-08 16:40:42 +0900

    n,m 으로 입력했을 때 2*1,... 2*m, ... n*1, ... , n*m 과 같이 출력하는 구구단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,10 +2,28 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		System.out.println("몇단을 출력할까요?: ");
+		System.out.println("어떻게 출력할까요?: ");
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-
-		System.out.println(n + " * " + n + " 단을 출력합니다.\n= " + n*n);
+		String inputValue = s.nextLine();
+		
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		if( first < 2 || second < 1 )
+		{
+			System.out.println("지원하지 않는 범위입니다.");
+		}
+		else
+		{
+			for(int i=2; i<first+1; i++)
+			{
+				System.out.println(i + "단을 출력합니다.");
+				for(int k=0; k<second; k++)
+				{
+					System.out.println(i + " * " + (k+1) + " = " + i*(k+1));
+				}
+			}
+		}
 	}
 }
